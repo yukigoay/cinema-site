7bac044b0721ff8ffde7309da3049d913d5a6662
Yuki Goay <yuki.goay@zchwantech.com>
2023-10-03 10:04:05 +0800

    add story book image

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as path from "path";
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      { find: "@", replacement: path.resolve(__dirname, "src") },
-      { find: "@css", replacement: path.resolve(__dirname, "src/css") },
-      { find: "@components", replacement: path.resolve(__dirname, "src/components") },
-      { find: "@assets", replacement: path.resolve(__dirname, "src/assets") },
-    ],
-  },
-})
+    plugins: [react()],
+    resolve: {
+        alias: [
+            { find: '@', replacement: path.resolve(__dirname, 'src') },
+            { find: '@css', replacement: path.resolve(__dirname, 'src/css') },
+            {
+                find: '@components',
+                replacement: path.resolve(__dirname, 'src/components'),
+            },
+            {
+                find: '@assets',
+                replacement: path.resolve(__dirname, 'src/assets'),
+            },
+            {
+                find: '@stories',
+                replacement: path.resolve(__dirname, 'src/stories'),
+            },
+        ],
+    },
+});
